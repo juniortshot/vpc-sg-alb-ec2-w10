460e34228d9d56fcd65d22af5acfd03fc079e715
tshot <juniortshot@gmail.com>
2025-09-06 11:23:43 -0400

    remove real name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraformstate-jmt"
+    bucket  = "replace here with your bucket name"
     key     = "alb/terraform.state"
     region  = "us-east-1"
     encrypt = true
